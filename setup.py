43466cf82bdf4881505697d9b93c9a7460f5059c
Kyle Altendorf <kyle.altendorf@epcpower.com>
2018-05-05 16:35:27 -0400

    another

--- setup.py
+++ setup.py
@@ -2,13 +2,7 @@ import setuptools
 
 
 setuptools.setup(
-    name="DataLake",
+    name="pkg",
     packages=setuptools.find_packages('src'),
     package_dir={'': 'src'},
-    install_requires=[
-        "click"
-    ],
-    entry_points={
-        "console_scripts": ["test = datalake.cli:main"]
-    },
 )
--- src/datalake/cli.py
+++
@@ -1,7 +0,0 @@
-import click
-
-
-@click.command()
-@click.option('-t', '--test', required=False)
-def main(test):
-    click.echo(f'i was executed: {test}')
