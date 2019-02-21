1a195d5d7186a220d85c5d5dfad229a90b59ebbf
Kyle Altendorf <kyle.altendorf@epcpower.com>
2019-02-21 09:50:04 -0500

    pip==19.0.3 setuptools==40.8.0, no failure

--- x.sh
+++ x.sh
@@ -1,6 +1,6 @@
 set -vx
 python3 -m venv venv
-venv/bin/python -m pip install --upgrade pip==19.0.1 setuptools==40.6.3
+venv/bin/python -m pip install --upgrade pip==19.0.3 setuptools==40.8.0
 rm pyproject.toml
 venv/bin/python -m pip install --ignore-installed .
 touch pyproject.toml
