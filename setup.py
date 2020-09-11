463da849e5fbfd6ed4df2df6e3edf54802f7ae5b
Thomas Grainger <tagrain@gmail.com>
2020-09-11 02:29:42 +0100

    run: `pipx run setup-py-upgrade .`

--- setup.py
+++ setup.py
@@ -1,17 +1,2 @@
-import setuptools
-
-
-setuptools.setup(
-    name="pkg",
-    packages=setuptools.find_packages('src'),
-    package_dir={'': 'src'},
-    entry_points={
-        'gui_scripts': [
-            'main = pkg.main:main',
-        ],
-    },
-    install_requires=[
-        'pyqt5',
-        'pyopengl',
-    ],
-)
+from setuptools import setup
+setup()
